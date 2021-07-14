a97a85e934bf618751ca2c48b3ce8d7ddeee6444
Matthias <xmatthias@outlook.com>
2021-07-14 11:16:19 +0200

    Update publish_docker_arm64.sh

--- build_helpers/publish_docker_arm64.sh
+++ build_helpers/publish_docker_arm64.sh
@@ -7,7 +7,8 @@ export DOCKER_BUILDKIT=1
 TAG=$(echo "${BRANCH_NAME}" | sed -e "s/\//_/g")
 TAG_PLOT=${TAG}_plot
 
-
+TAG_ARM=${TAG}_arm
+TAG_PLOT_ARM=${TAG_PLOT}_arm
 
 echo "Running for ${TAG}"
 
@@ -17,13 +18,13 @@ echo "${GITHUB_SHA}" > freqtrade_commit
 if [ "${GITHUB_EVENT_NAME}" = "schedule" ]; then
     echo "event ${GITHUB_EVENT_NAME}: full rebuild - skipping cache"
     # Build regular image
-    docker build -t freqtrade:${TAG} .
+    docker build -t freqtrade:${TAG_ARM} .
 
 else
     echo "event ${GITHUB_EVENT_NAME}: building with cache"
     # Build regular image
-    docker pull ${IMAGE_NAME}:${TAG}
-    docker build --cache-from ${IMAGE_NAME}:${TAG} -t freqtrade:${TAG} .
+    docker pull ${IMAGE_NAME}:${TAG_ARM}
+    docker build --cache-from ${IMAGE_NAME}:${TAG_ARM} -t freqtrade:${TAG_ARM} .
 
 fi
 
@@ -34,12 +35,12 @@ fi
 # Tag image for upload and next build step
 docker tag freqtrade:$TAG ${IMAGE_NAME}:$TAG
 
-docker build --cache-from freqtrade:${TAG} --build-arg sourceimage=${TAG} -t freqtrade:${TAG_PLOT} -f docker/Dockerfile.plot .
+docker build --cache-from freqtrade:${TAG_ARM} --build-arg sourceimage=${TAG_ARM} -t freqtrade:${TAG_PLOT_ARM} -f docker/Dockerfile.plot .
 
-docker tag freqtrade:$TAG_PLOT ${IMAGE_NAME}:$TAG_PLOT
+docker tag freqtrade:$TAG_PLOT_ARM ${IMAGE_NAME}:$TAG_PLOT_ARM
 
 # Run backtest
-docker run --rm -v $(pwd)/config_bittrex.json.example:/freqtrade/config.json:ro -v $(pwd)/tests:/tests freqtrade:${TAG} backtesting --datadir /tests/testdata --strategy-path /tests/strategy/strats/ --strategy DefaultStrategy
+docker run --rm -v $(pwd)/config_bittrex.json.example:/freqtrade/config.json:ro -v $(pwd)/tests:/tests freqtrade:${TAG_ARM} backtesting --datadir /tests/testdata --strategy-path /tests/strategy/strats/ --strategy DefaultStrategy
 
 if [ $? -ne 0 ]; then
     echo "failed running backtest"
@@ -49,15 +50,19 @@ fi
 docker images
 
 # docker push ${IMAGE_NAME}
-docker push ${IMAGE_NAME}:$TAG_PLOT
-docker push ${IMAGE_NAME}:$TAG
+docker push ${IMAGE_NAME}:$TAG_PLOT_ARM
+docker push ${IMAGE_NAME}:$TAG_ARM
 
 # Create multiarch image
 # Make sure that all images contained here are pushed to github first.
 # Otherwise installation might fail.
 
-# docker manifest create freqtradeorg/freqtrade:${TAG} ${IMAGE_NAME}:${TAG} ${IMAGE_NAME}:${TAG_PI}
-# docker manifest push freqtradeorg/freqtrade:${TAG}
+docker manifest create freqtradeorg/freqtrade:${TAG} freqtradeorg/freqtrade:${TAG_ARM} ${IMAGE_NAME}:${TAG} ${IMAGE_NAME}:${TAG_PI}
+docker manifest push freqtradeorg/freqtrade:${TAG}
+
+
+docker manifest create freqtradeorg/freqtrade:${TAG_PLOT} freqtradeorg/freqtrade:${TAG_PLOT_ARM} ${IMAGE_NAME}:${TAG}
+docker manifest push freqtradeorg/freqtrade:${TAG_PLOT}
 
 # Tag as latest for develop builds
 # if [ "${TAG}" = "develop" ]; then
